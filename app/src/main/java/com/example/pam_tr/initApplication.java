9a73b0fd84a1259506e85f4286aad88f1a13b624
Gedalya <37411477+ChoiDaHye@users.noreply.github.com>
2020-04-10 00:00:46 +0700

    update 7

--- app/src/main/java/com/example/pam_tr/initApplication.java
+++ app/src/main/java/com/example/pam_tr/initApplication.java
@@ -5,23 +5,27 @@ import android.app.Application;
 public class initApplication extends Application {
     private static initApplication singleton = null;
     private boolean isNightModeEnabled = false;
-    public static initApplication getInstance(){
-        if (singleton==null) {
-        singleton = new initApplication();
+
+    public static initApplication getInstance() {
+        if (singleton == null) {
+            singleton = new initApplication();
         }
         return singleton;
-        }
-@Override
-public void onCreate(){
+    }
+
+    @Override
+    public void onCreate() {
         super.onCreate();
-        singleton=this;
-}
-public boolean isNightModeEnabled(){
+        singleton = this;
+    }
+
+    public boolean isNightModeEnabled() {
         return isNightModeEnabled;
 
-}
-public void  setNightModeEnabled (boolean n){
-        this.isNightModeEnabled=n;
-}
+    }
+
+    public void setNightModeEnabled(boolean n) {
+        this.isNightModeEnabled = n;
+    }
 
 }
